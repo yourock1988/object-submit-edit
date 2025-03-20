e38fb3a623e5f7b3d457d3634070cce130f91911
tolik-85 <tolian8576@gmail.com>
2025-03-20 16:44:58 +0200

    add inputs for adding object car by user

--- src/components/widget-cars/CarSubmitter.vue
+++ src/components/widget-cars/CarSubmitter.vue
@@ -1,13 +1,36 @@
 <script>
 export default {
   emits: ['car-submitted'],
+  data() {
+    return {
+      car: {
+        brand: '',
+        price: '',
+      },
+    }
+  },
+  methods: {
+    addCar() {
+      console.log(this.car)
+
+      this.$emit('car-submitted', { ...this.car })
+      this.car.brand = ''
+      this.car.price = ''
+    },
+  },
 }
 </script>
 
 <template>
-  <button @click="$emit('car-submitted', { brand: 'BMW', price: 33000 })">
-    Submit
-  </button>
+  <div>
+    <span>Car brand</span>
+    <input type="text" v-model="car.brand" />
+  </div>
+  <div>
+    <span>Car Price</span>
+    <input type="number" v-model="car.price" />
+  </div>
+  <button @click="addCar">Submit</button>
 </template>
 
 <!-- сделать чтоб свойства можно было вводить руками -->
